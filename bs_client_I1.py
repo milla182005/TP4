d7b434374c7d2e501a0b4e806f000c2fac2d1a6e
lingrid <ingrid.lare@ynov.com>
2024-10-18 16:07:22 +0200

    new files

---
+++ bs_client_I1.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+
+host = '10.1.1.20'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+try:
+          
+         with s.connect((host, port)) :
+
+              s.sendall(b'Meoooo !')
+
+              data = s.recv(1024)
+              if data:
+               print(f"Le serveur a répondu {repr(data)}")
+         sys.exit(0)
+
+except Exception as e:
+   print(f"Error : {e}")
+
+   sys.exit(1)
+
