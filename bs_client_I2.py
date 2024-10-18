f529ff7a492acd3491abdffd4391cd8e4b4ee8cf
lingrid <ingrid.lare@ynov.com>
2024-10-18 17:25:54 +0200

    test

---
+++ bs_client_I2.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+host = '10.1.1.2'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+try:
+    s.sendall("Connecté avec succès au serveur {host} sur le port {port}".encode())
+
+    user_input = input("Que veux-tu envoyer au serveur : ")
+    s.sendall(user_input.encode())
+
+    data = s.recv(1024)
+    if data:
+        print(f"Réponse du serveur : {repr(data)}")
+
+        sys.exit(0)
+
+except Exception as e:
+   print(f"Error :Impossible de se connecter au serveur {host} sur le port {port}. Détails {e}")
+
+   sys.exit(1)
+
---
+++ bs_server_I2.py
@@ -0,0 +1,42 @@
+import socket
+
+host = '10.1.1.2'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+
+print(f"Serveur en écoute sur {host}:{port}")
+
+
+while True:
+        s.listen(1)
+        conn, addr = s.accept()
+
+        print(f"Un client vient de se co et son IP c'est {addr[0]}.")
+
+        conn.sendall(b"Hi mate !")
+
+        try:
+            data = conn.recv(1024)
+
+            if not data: break
+        
+            client_message = data.decode()
+            print(f"Message reçu du client: {client_message.encode()} : {data}")
+
+            if "meo" in client_message.lower():
+                        response = "Meo à toi confrère."
+            elif "waf" in client_message.lower():
+                        response = "ptdr t ki"
+            else:
+                        response = "Mes respects humble humain."
+
+            conn.sendall(response.encode())
+
+        except socket.error:
+          print("Erreur lors de la réception du message.")
+
+conn.close()
+
