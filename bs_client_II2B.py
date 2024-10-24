a29693ea3361f1a9a4dcf18216227290a322abac
lingrid <ingrid.lare@ynov.com>
2024-10-24 17:32:16 +0200

    nouveau fichier

---
+++ bs_client_II2B.py
@@ -0,0 +1,29 @@
+import logging
+import socket
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler('/tmp/bs_client.log') 
+    ]
+)
+
+def connect_to_server(ip, port, message):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((ip, port))
+            logging.info(f"Connexion réussie à {ip}:{port}.")
+            
+        
+            client_socket.sendall(message.encode())
+            logging.info(f"Message envoyé au serveur {ip}:{port} : \"{message}\".")
+           
+            response = client_socket.recv(1024).decode()
+            logging.info(f"Réponse reçue du serveur {ip}:{port} : \"{response}\".")
+    except ConnectionRefusedError:
+        logging.error(f"Impossible de se connecter au serveur {ip} sur le port {port}.")
+
+if __name__ == "__main__":
+    connect_to_server('127.0.0.1', 8888, "Hello, Server!") 
