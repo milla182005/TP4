6161bca97826b4e715eb806c190a6905ffb2c890
lingrid <ingrid.lare@ynov.com>
2024-10-25 17:51:33 +0200

    update tp4

---
+++ bs_client_III.py
@@ -0,0 +1,65 @@
+import socket
+import sys
+import argparse
+from datetime import datetime
+
+
+def check_port(port):
+    port = int(port)
+    if port < 1024 or port > 65535:
+        print("Le port doit être entre 1024 et 65535.")
+    return port
+
+def check_ip(ip):
+    return ip
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-p", "--port", type=check_port, default=13337, help="Le port sur lequel le serveur écoute (par défaut 13337)")
+
+parser.add_argument("-l", "--listen", type=check_ip, required=True, help="L'adresse IP sur laquelle le serveur écoute")
+
+args = parser.parse_args()
+
+host = args.listen
+port = args.port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+try:
+    data = s.recv(1024)
+
+    print(f"{data.decode()}")
+
+    user_input = input("Que veux-tu envoyer au serveur : ")
+    s.sendall(user_input.encode())
+
+    validate_expression = data.encode()
+
+    data = s.recv(1024)
+    while True:
+            user_input = input("Entrez une opération arithmétique (ou 'exit' pour quitter) : ")
+            if user_input.lower() == 'exit':
+                break
+            
+            if validate_expression(user_input):
+                s.sendall(user_input.encode())
+                data = s.recv(1024)
+                if data:
+                    print(f"Réponse du serveur : {data.decode()}")
+            else:
+                print("Expression invalide. Veuillez entrer une opération sous la forme : 'a op b' (où a et b sont des entiers et op est +, -, ou *).")
+
+except Exception as e:
+   print(f"Error :Impossible de se connecter au serveur {host} sur le port {port}. Détails {e}")
+
+   sys.exit(1)
+
+   def log(msg: str, log_level: str):
+           now = datetime.now()
+           with open('/tmp/bs_client_II2B.log', "a") as log_file:
+             log_file.write(f"{now} {log_level} {msg}\n")
+log("INFO", "Connexion réussie à <IP>:<PORT>.")
+
+
---
+++ bs_server_III.py
@@ -0,0 +1,72 @@
+import socket
+import argparse
+from datetime import datetime
+from sys import exit as sysexit
+
+def check_port(port):
+    port = int(port)
+    if port < 1024 or port > 65535:
+        print("Le port doit être entre 1024 et 65535")
+        sysexit(1)
+    return port
+
+def check_ip(ip):
+    return ip
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-p", "--port", type=check_port, default=13337, help="Le port sur lequel le serveur écoute (par défaut 13337)")
+
+parser.add_argument("-l", "--listen", type=check_ip, required=True, help="L'adresse IP sur laquelle le serveur écoute")
+
+args = parser.parse_args()
+
+host = args.listen
+port = args.port
+
+print(f"Le serveur va écouter sur l'adresse {args.listen} et le port {args.port}.")
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+
+print(f"Serveur en écoute sur {host}:{port}")
+
+
+while True:
+        s.listen(1)
+        conn, addr = s.accept()
+
+        print(f"Un client vient de se co et son IP c'est {addr[0]}.")
+        
+        conn.sendall(b"Hi mate !")
+
+        try:
+            data = conn.recv(1024)
+            if not data: break
+            client_message = data.decode()
+            print(f"Message reçu du client: {client_message}")
+
+            client_message = data.decode()
+            try:
+                       
+                result = eval(client_message)
+                response = str(result)
+                conn.sendall(response.encode())
+            except Exception as e:
+                response = f"Erreur dans l'évaluation : {str(e)}"
+
+        except socket.error:
+          print("Erreur lors de la réception du message.")
+
+          def log(msg: str, log_level: str):
+           now = datetime.now()
+           with open('/tmp/bs_client_II2B.log', "a") as log_file:
+             log_file.write(f"{now} {log_level} {msg}\n")
+log("INFO", "Le serveur tourne sur <IP>:<port>")
+
+
+conn.close()
+
